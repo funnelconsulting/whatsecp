e93bd8db2ef40c54229c198b17af82776326a04a
Mattia Noris <100127022+Mattia23Dev@users.noreply.github.com>
2024-03-08 14:51:57 +0100

    OK

---
+++ ECP.js
@@ -0,0 +1,202 @@
+exports.ECP = [
+    {
+      _id: "64e5c95af74004ce77ee8170", 
+      name:"ARCHIMEDE FORMAZIONE SRL", 
+      groupName: "LeadSystem - Archimede Formazione", 
+      waId: {
+        server: 'g.us',
+        user: '120363254016044512',
+        _serialized: '120363254016044512@g.us'
+      },
+    },
+    {
+        _id: "65cb10d373507e4c8f22a94e", 
+        name:"Centro Studi ABC", 
+        groupName: "LeadSystem - Centro Studi ABC", 
+        waId: {
+            server: 'g.us',
+            user: '120363253382168534',
+            _serialized: '120363253382168534@g.us'
+        },
+      },
+      {
+        _id: "65cdc12d2f10eb0e144f1a8f", 
+        name:"CSUV", 
+        groupName: "LeadSystem - CSUV", 
+        waId: {
+            server: 'g.us',
+            user: '120363233247203756',
+            _serialized: '120363233247203756@g.us'
+        },
+      },
+      {
+        _id: "65afa267e4ba456784766915", 
+        name:"DICHE SRLS", 
+        groupName: "LeadSystem - Diche", 
+        waId: {
+            server: 'g.us',
+            user: '120363254486664617',
+            _serialized: '120363254486664617@g.us'
+        },
+      },
+      {
+        _id: "651a9fd17ccf14d8d808b876", 
+        name:"EXEDU Srl", 
+        groupName: "LeadSystem - EXEDU", 
+        waId: {
+            server: 'g.us',
+            user: '120363236425060903',
+            _serialized: '120363236425060903@g.us'
+        },
+      },
+      {
+        _id: "64e378c8645bd5e764903d58", 
+        name:"Fabricamente Srl", 
+        groupName: "LeadSystem - Fabricamente", 
+        waId: {
+            server: 'g.us',
+            user: '120363236480504398',
+            _serialized: '120363236480504398@g.us'
+        },
+      },
+      {
+        _id: "65cb9931771c0987d9b77283", 
+        name:"Gavì Education Srl", 
+        groupName: "LeadSystem - Gavì Education", 
+        waId: {
+            server: 'g.us',
+            user: '120363237875241506',
+            _serialized: '120363237875241506@g.us'
+        },
+      },
+      {
+        _id: "659ffb6cbcaec93ab33f3776", 
+        name:"Gift Srls", 
+        groupName: "LeadSystem - Gift", 
+        waId: {
+            server: 'g.us',
+            user: '120363253516818356',
+            _serialized: '120363253516818356@g.us'
+        },
+      }, 
+      {
+        _id: "656d9b0290c49696ca8c73e6", 
+        name:"Ippocrene S.r.l.", 
+        groupName: "LeadSystem - Ippocrene", 
+        waId: {
+            server: 'g.us',
+            user: '120363236857487183',
+            _serialized: '120363236857487183@g.us'
+        },
+      },
+      {
+        _id: "65ca16f1f4ccaaa1349c6529", 
+        name:"LOVISS FROMAZIONE SRL", 
+        groupName: "LeadSystem - Loviss Formazione", 
+        waId: {
+            server: 'g.us',
+            user: '120363253968123621',
+            _serialized: '120363253968123621@g.us'
+        },
+      }, 
+      {
+        _id: "651ad6467ccf14d8d80a61bd", 
+        name:"maurizio peddis", 
+        groupName: "LeadSystem - Peddis", 
+        waId: {
+            server: 'g.us',
+            user: '120363233728895020',
+            _serialized: '120363233728895020@g.us'
+        },
+      }, 
+      {
+        _id: "65e746abd0854d9a924c4f56", 
+        name:"MY FUTURE INNOVATION ITALIA SRL", 
+        groupName: "LeadSystem - My future Innovation Italia", 
+        waId: {
+            server: 'g.us',
+            user: '120363253739935454',
+            _serialized: '120363253739935454@g.us'
+        },
+      },
+      {
+        _id: "655f707143a59f06d5d4dc3b", 
+        name:"One Network SRL", 
+        groupName: "LeadSystem - Orientamee", 
+        waId: {
+            server: 'g.us',
+            user: '120363253564022455',
+            _serialized: '120363253564022455@g.us'
+        },
+      },
+      {
+        _id: "651acf7e7ccf14d8d809fc5c", 
+        name:"Piccola Università Italiana", 
+        groupName: "LeadSystem - Piccola Università Italiana", 
+        waId: {
+            server: 'g.us',
+            user: '120363255141208847',
+            _serialized: '120363255141208847@g.us'
+        },
+      },
+      {
+        _id: "656afbba90c49696ca89b4da", 
+        name:"Polo Formativo San Giuseppe srls", 
+        groupName: "LeadSystem - Polo Formativo San Giuseppe", 
+        waId: {
+            server: 'g.us',
+            user: '120363236910061060',
+            _serialized: '120363236910061060@g.us'
+        },
+      },
+      {
+        _id: "65bb7d304e9a4c64bef07aab", 
+        name:"SCUOLA BUONA", 
+        groupName: "LeadSystem - Scuola Buona", 
+        waId: {
+            server: 'g.us',
+            user: '120363236424127495',
+            _serialized: '120363236424127495@g.us'
+        },
+      },
+      {
+        _id: "64e37194645bd5e7648f799c", 
+        name:"Sparta Formazione S.r.l.", 
+        groupName: "LeadSystem - Sparta Formazione", 
+        waId: {
+            server: 'g.us',
+            user: '120363254612239714',
+            _serialized: '120363254612239714@g.us'
+        },
+      },
+      {
+        _id: "65c4a1cc63ecf824d1779ccf", 
+        name:"universibusto srl", 
+        groupName: "LeadSystem - Universibusto", 
+        waId: {
+            server: 'g.us',
+            user: '120363255620835418',
+            _serialized: '120363255620835418@g.us'
+        },
+      },
+      {
+        _id: "64bfcaf5d6a54ecbab8dcfea", 
+        name:"Uniweb srl", 
+        groupName: "LeadSystem - Uniweb", 
+        waId: {
+            server: 'g.us',
+            user: '120363254150449147',
+            _serialized: '120363254150449147@g.us'
+        },
+      },
+      {
+        _id: "65b135d2318336cd0bfc4361", 
+        name:"Wikibe SRL", 
+        groupName: "LeadSystem - Arco", 
+        waId: {
+            server: 'g.us',
+            user: '120363254020621632',
+            _serialized: '120363254020621632@g.us'
+        },
+      },
+  ]
---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+const path = require("path");
+const { Client, LocalAuth, Poll, List } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const { fstat } = require('fs');
+const { ECP } = require('./ECP');
+const client = new Client({
+    authStrategy: new LocalAuth({
+        dataPath: 'dataSession'
+    }),
+    puppeteer: {
+      args: [
+          '--no-sandbox',
+          //'--disable-setuid-sandbox',
+      ]
+    }
+  });
+ //      headless: true,
+const app = express();
+app.use(express.static(path.join(__dirname, 'client', 'public')));
+
+app.use(express.json({ limit: "10mb" }));
+app.use(
+  cors({
+    origin: ["https://test-comparatore.netlify.app", "https://leadsystem-test.netlify.app", "http://localhost:3000", "https://leadsystemfunnel-production.up.railway.app"],
+  })
+);
+
+const port = 8080;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+/*WHATSAPP CONNECTION*/
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+  });
+  
+client.on('ready', () => {
+  console.log('Client is ready!');
+  console.log(client);
+  client.getChats().then((chats) => {
+    const group1 = chats.filter(c => c.name === "LeadSystem - Arco");
+    //console.log(group1);
+  }).catch((err) => {
+      console.error('Si è verificato un errore durante la ricerca della chat:', err);
+  });
+});
+
+app.post('/webhook-lead-ecp-notification', async (req, res) => {
+  try {
+      const ecpWithLeads = req.body.ecpLeadTracking; 
+
+      for (const ecp of ecpWithLeads) {
+        const { ecpId, leads } = ecp;
+        console.log(leads)
+        const knownEcp = ECP.find(item => item._id === ecpId);
+          if (knownEcp) {
+            for (const lead of leads) {
+              const leadMessage = `È entrata una nuova lead! contattala subito.\n• ${lead.nome} ${lead.cognome} - ${lead.numeroTelefono}`;
+              const { waId } = knownEcp;
+  
+              await client.sendMessage(waId._serialized, leadMessage)
+                  .then(() => console.log("Messaggio inviato a", knownEcp.name, "per la lead:", lead.nome, lead.cognome))
+                  .catch(error => console.error("Errore nell'invio del messaggio:", error));
+          }
+              console.log(`Messaggio inviato a ${knownEcp.name}`);
+          } else {
+              console.log(`ECP non trovato con id: ${ecp.nameECP}`);
+          }
+      }
+      res.status(200).send('Messaggi inviati con successo agli ECP.');
+  } catch (error) {
+      console.error('Errore durante l\'invio dei messaggi:', error);
+      res.status(500).send('Errore durante l\'invio dei messaggi.');
+  }
+});
+  
+  client.initialize();
