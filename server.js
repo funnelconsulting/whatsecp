5573408c6f5c7b0de947da9e6a8c3d83e4074230
Mattia Noris <100127022+Mattia23Dev@users.noreply.github.com>
2024-04-03 10:27:42 +0200

    ook

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const client = new Client({
     authStrategy: new LocalAuth({
         dataPath: 'dataSession'
     }),
+    webVersionCache:{ 
+      type: 'remote', remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html', 
+    }, 
     puppeteer: {
       headless: true,
       args: [
